065bc73aa00ac27235e91aeb8e9eb41e4c0715fd
John <wwwdotphilip@gmail.com>
2018-02-16 19:40:15 +0800

    Login screen

--- app/src/main/java/com/juan/lazy/gasolina/LoginActivity.java
+++ app/src/main/java/com/juan/lazy/gasolina/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.juan.lazy.gasolina;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void SignIn(View view){
+        startActivity(new Intent(LoginActivity.this, MapsActivity.class));
     }
 }
